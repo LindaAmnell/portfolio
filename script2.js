cefb6f024177270d60d91fb0fe20192357076cd0
LindaAmnell <amnell3@hotmail.com>
2024-02-29 19:56:40 +0100

    jobbat med mobilläget

---
+++ script2.js
@@ -0,0 +1,22 @@
+// // Hämta knappen och ytterligare informationselementet
+// const showInfoButton = document.getElementById("show-info");
+// const additionalInfo = document.getElementById("additional-info");
+
+// // Lägg till händelselyssnare för klickhändelser på knappen
+// showInfoButton.addEventListener("click", function (event) {
+//   // Förhindra standardbeteendet för knappen
+//   event.preventDefault();
+
+//   // Kolla om ytterligare information redan är synlig
+//   if (additionalInfo.style.display === "block") {
+//     // Dölj ytterligare information om den redan är synlig
+//     additionalInfo.style.display = "none";
+//     // Ändra knappens text tillbaka till ursprunglig text
+//     showInfoButton.textContent = "Visa mer information";
+//   } else {
+//     // Visa ytterligare information om den är dold
+//     additionalInfo.style.display = "block";
+//     // Ändra knappens text
+//     showInfoButton.textContent = "Dölj information";
+//   }
+// });
